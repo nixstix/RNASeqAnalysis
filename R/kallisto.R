3e60d0c8aa2bbe5d516f8064d37261f6c691225a
Nicky <nixthrupp@gmail.com>
2016-01-21 16:48:31 +0100

    updated kallistoIndex function

--- R/kallisto.R
+++ R/kallisto.R
@@ -1,9 +1,9 @@
-#' A number of transcriptome references have already been built, for use with \code{kallistoQuant}
+#' A number of transcriptome indices have already been built, for use with \code{kallistoQuant}
 #' 
-#' DESCRIPTION GOES HERE
+#' A number of transcriptome index files (.idx files) have been pre-built and provided with the package. These files may be used as input to the \code{kallsitoQuant} pseudoalignment function (or the user may provide his/her own transcriptome file). The \code{availableReferences} function provides a list of these ready-available transcriptomes.   
 #' 
 #' @param None No parameters
-#' @return A list of available transcriptomes (.idx files)
+#' @return A list of available transcriptome indices (.idx files)
 #' @examples 
 #' availableReferences()
 #'  
@@ -11,17 +11,19 @@
 availableReferences <- function(){
         extdata.dir <- system.file("extdata", package="RNASeqAnalysis")
         extdata.dir <- list.files(extdata.dir)
-        extdata.dir <- grep("kallisto.idx", extdata.dir, value = TRUE)
+        extdata.dir <- grep(".idx", extdata.dir, value = TRUE)
+        return(as.list(extdata.dir))
+        
 }
 
-#' Builds a transcriptome index from a FASTA formatted file of target sequences
+#' Builds a transcriptome index (.idx) from a FASTA-formatted transcriptome.
 #' 
-#' DESCRIPTION GOES HERES. Kallisto must be installed locally. Kallisto can be downloaded from \url{https://pachterlab.github.io/kallisto/download.html}.
+#' \code{kallistoIndex} builds an index of  Once built, the transcriptome index may be used for pseudoalignment of the target RNA-Seq data in order to quantify expression (see \code{kallistoQuant}). Kallisto must be installed locally. Kallisto can be downloaded from \url{https://pachterlab.github.io/kallisto/download.html}.
 #' @param refTranscriptome A character string of the transcriptome file (in
 #'   fasta format) to be indexed.
 #' @param indexName A character string of the output index file to be created.
 #'   Default is "./transcripts.idx").
-#' @return An index file which can be used to quantify expression data using
+#' @return An index file (.idx) which can be used to quantify expression data using
 #'   \code{\link{kallistoQuant}} pseudoalignment.
 #' @examples
 #' kallistoIndex("./transcripts.fasta")
@@ -35,17 +37,13 @@ kallistoIndex <- function(refTranscriptome, indexName="./transcripts.idx"){
     system(cmd)
 }
 
-
-# NOTE TO DO:
-# create index using transcriptomes supplied by the package
-
 #Quantifies transcript expression
 #'
 #'DESCRIPTION GOES HERE
 #'
 #' @param file1 A character string of the name of the (fastq.gz) file to be processed.
 #' @param file2 A character string of the name of the second (fastq.gz) file to be processed. This is used in the case that paired-end data is available. 
-#' @param refIndex A character string of the name of the index file (usually, .idx) against which the fastq files are pseudoaligned. This file can be generated from a transcriptome (fasta) file using \code{\link{kallistoIndex}}.
+#' @param refIndex A character string of the name of the index file (usually, .idx) against which the fastq files are pseudoaligned. This file can be generated from a fasta-formatted transcriptome file using \code{\link{kallistoIndex}}.
 #' @param pairedEnd is a logical. If \code{true} (default), a paired end protocol is chosen (for this, the file1 and file2 parameters must be specified). If \code{false}, a single end protocol will be run, and only file1 will be processed.
 #' @param bootstrap An integer specifying the number of times MORE.
 #' @param fragmentLength An integer. MORE.
