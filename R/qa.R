faa5a0d29dec21babfa31c4a1af0a27549354470
Nicky <nixthrupp@gmail.com>
2016-06-08 16:36:47 +0200

    function returns FastqQA object

--- R/qa.R
+++ R/qa.R
@@ -4,7 +4,7 @@
 #' 
 #' @param dataFile An R data frame with the data to be processed. The R object is a standard format, and must contain the following headings: File, PE, Sample, Replicate, FilteredFile. More information about the file is available at \code{datafileTemplate}. 
 #' @param preFilter A logical - if true (default), the function will select and analyse files which have not yet been processed for quality. If false, the function will select and analyse those files which have been processed for quality, i.e. the "filtered file" in the data file.
-#' @return Outputs directories with quality results in the form of raw data and HTLM format
+#' @return FastqQA object. Outputs quality results in the form of raw data (an R FastqQA object) and HTML format (saved to "QA" directory).
 #' @details The function should be run in the working directory, where all fastq files are found.
 #' @details \code{runQA} iterates over each file specified in the "datafile". It runs a quality assessment from the \code{ShortRead} package. The \code{ShortRead} package (\url{https://bioconductor.org/packages/release/bioc/html/ShortRead.html}) contains more information about this step. The quality assessment may be performed before and after the filtering step, by setting the "pre-filter" parameter to true or to false, respectively. All quality assessment data is output to the "QA" directory. 
 #' Quality reports are output to the working directory (under the QA directory). R objects of the raw data used to generate the reports are also saved to this directory. 
@@ -15,15 +15,16 @@ runQA <- function(dataFile, preFilter = TRUE){
         
         if (preFilter == TRUE){ 
                 # pre-filter quality check
-                QASum_prefilter <- qa(dirPath = dataFile$FILE, type = "fastq")
+                QASum_filter <- qa(dirPath = dataFile$FILE, type = "fastq")
                 QASum_prefilterRpt <- report(x = QASum_prefilter, dest = "QA/prefilter", type = "html")
                 save(QASum_prefilter, file = "./QA/prefilter/QASum_prefilter.RData")
         } 
         
         else if(preFilter == FALSE){
                 # pre-filter quality check
-                QASum_postfilter <- qa(dirPath = unique(dataFile$FILTEREDFILE), type = "fastq")
+                QASum_filter <- qa(dirPath = unique(dataFile$FILTEREDFILE), type = "fastq")
                 QASum_postfilterRpt <- report(x = QASum_postfilter, dest = "QA/postfilter/", type = "html")
                 save(QASum_postfilter, file = "./QA/postfilter/QASum_postfilter.RData")        
         }
+        return(QASum_filter)
 }
