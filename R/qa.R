eca0712bfd3ad904b69d0cf0fdffc8696414c2d7
Nicky <nixthrupp@gmail.com>
2016-07-01 15:08:16 +0200

    parallelised code, updated documentation

--- R/qa.R
+++ R/qa.R
@@ -4,18 +4,21 @@
 #' 
 #' @param dataFile An R data frame with the data to be processed. The R object is a standard format, and must contain the following headings: File, PE, Sample, Replicate, FilteredFile. More information about the file is available at \code{datafileTemplate}. 
 #' @param preFilter A logical - if true (default), the function will select and analyse files which have not yet been processed for quality. If false, the function will select and analyse those files which have been processed for quality, i.e. the "filtered file" in the data file.
+#' @param mc.cores A number specifying the number of cores to be used during parallel processing. 
 #' @return FastqQA object. Outputs quality results in the form of raw data (an R FastqQA object) and HTML format (saved to "QA" directory).
 #' @details The function should be run in the working directory, where all fastq files are found.
 #' @details \code{runQA} iterates over each file specified in the "datafile". It runs a quality assessment from the \code{ShortRead} package. The \code{ShortRead} package (\url{https://bioconductor.org/packages/release/bioc/html/ShortRead.html}) contains more information about this step. The quality assessment may be performed before and after the filtering step, by setting the "pre-filter" parameter to true or to false, respectively. All quality assessment data is output to the "QA" directory. 
 #' Quality reports are output to the working directory (under the QA directory). R objects of the raw data used to generate the reports are also saved to this directory. 
 #' @export
 #' @import ShortRead
-runQA <- function(dataFile, preFilter = TRUE, mc.cores){
+runQA <- function(dataFile, preFilter = TRUE, mc.cores = NULL){
         
         if(!missing(mc.cores)){
                 BPPARAM = MulticoreParam(workers = mc.cores)
-                }
-        
+        }
+        else{
+                BPPARAM = registered()[1]
+        }
         
         print("QA results will be output to the 'QA' folder")
         
